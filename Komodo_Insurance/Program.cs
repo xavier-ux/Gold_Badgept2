3662c280bac872e9c33307b70dcb6379539ed72e
xavier-ux <xcollins90@gmail.com>
2020-07-15 14:51:21 -0400

    First commit

---
+++ Komodo_Insurance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komodo_Insurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ui = new ProgramUI();
+            ui.Run();
+        }
+    }
+}
